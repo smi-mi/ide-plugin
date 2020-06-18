28cdfe5dd10bec479e0c503ee49177440dadd9bf
Mariia <smimi_00@mail.ru>
2020-06-18 22:38:05 +0300

    Fold action if text is not selected.

--- src/main/java/SearchErrorAction.java
+++ src/main/java/SearchErrorAction.java
@@ -1,29 +1,22 @@
 import com.intellij.ide.BrowserUtil;
-import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class SearchErrorAction extends AnAction {
 
     @Override
+    public void update(@NotNull AnActionEvent e) {
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = editor.getCaretModel();
+        e.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
+    }
+
+    @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        /*URI uri = null;
-        try {
-            uri = new URI("https://google.com");
-            Desktop desk = Desktop.getDesktop();
-            desk.browse(uri);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }*/
 
         Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
         CaretModel caretModel = editor.getCaretModel();
