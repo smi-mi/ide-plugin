9e8a41b083ea6555d625adca46db1f27faf81af9
Mariia <smimi_00@mail.ru>
2020-06-18 18:35:53 +0300

    Create empty action.

---
+++ src/main/java/SearchErrorAction.java
@@ -0,0 +1,11 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class SearchErrorAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        // TODO implement
+    }
+}
